a70da13c30e79ce59f3bcbca3c3331344bc28460
Anders <dom.galbavy@gmail.com>
2024-01-06 22:33:00 +0100

    centered content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	export let data: PageServerData
 </script>
 
-<div class="flex-1 overflow-scroll w-full max-w-screen-sm flex gap-4 flex-col p-4">
+<div class="flex-1 overflow-scroll m-auto w-full max-w-screen-sm flex gap-4 flex-col p-4">
 	{#each data.occupancies as occ}
 		<Bar name={occ.name} occupancy={occ.occupancy} />
 	{:else}
