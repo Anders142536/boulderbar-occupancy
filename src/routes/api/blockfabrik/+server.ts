e7f86a56c2e0e78f28952811f15cf7043bbfa595
Anders <dom.galbavy@gmail.com>
2024-01-11 01:04:58 +0100

    added api endpoints for boulder halls

---
+++ src/routes/api/blockfabrik/+server.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import type { RequestHandler } from "./$types";
+import { json } from "@sveltejs/kit";
+
+type BlockFabrikData = {
+	counter: number
+	maxcount: number
+	countername: string
+}
+
+export const GET: RequestHandler = async ({ url }) => {
+	// was hardcoded on their website
+	const fetchUrl =
+		'https://www.boulderado.de/boulderadoweb/gym-clientcounter/index.php?mode=get&token=eyJhbGciOiJIUzI1NiIsICJ0eXAiOiJKV1QifQ.eyJjdXN0b21lciI6IkJsb2NrZmFicmlrV2llbiJ9.yymz1Eg_-jX28iMdaq1aGVb0iD4-29uWVkuxZd7a_9U&raw=1'
+
+	const response = await axios.get(fetchUrl)
+	const res = response.data as BlockFabrikData
+	console.debug(`res: ${JSON.stringify(res)}`)
+
+	const maxCount = 195 // hardcoded on their website
+	const occ = ((res.counter / maxCount) * 100).toFixed(0) // same way as on their website
+	console.log(`loaded blockfabrik with ${occ.padStart(3)}%`)
+	return json({
+		occupancy: Number(occ)
+	})
+}
---
+++ src/routes/api/boulderbar/+server.ts
@@ -0,0 +1,41 @@
+import { error, json } from "@sveltejs/kit"
+import type { RequestHandler } from "./$types"
+import axios from "axios"
+
+const axNew = axios.create({
+	baseURL: 'https://flash-cloud.boulderbar.net/modules/bbext/'
+})
+
+const axOld = axios.create({
+	baseURL: 'https://flash-cloud-sbg.boulderbar.net/modules/bbext/'
+})
+
+
+export const GET: RequestHandler = async ({ url }) => {
+	const tag = url.searchParams.get('tag')
+
+	if (tag === null) {
+		error(400, 'The tag of the requested boulderbar must be given')
+	}
+
+	const useOld = Boolean(url.searchParams.get('useOld')) ?? false
+	const axInstanceToUse = useOld ? axOld : axNew
+
+	const response = await axInstanceToUse.get(`CustomerCapacity.php?gym=${tag}`)
+	const resData = response.data as string
+
+	// ~~~magic~~~
+	const h2Regex = /<h2>(\d*)%<\/h2>/g
+
+	const matches = h2Regex.exec(resData)
+
+	if (matches === null) {
+		console.debug('matches was null, something failed')
+		error(500, 'Could not parse occupancy')
+	}
+
+	console.debug(`regex matched: ${matches[0]} with parsed number ${matches[1]}`)
+	return json({
+		occupancy: Number(matches[1])
+	})
+}
